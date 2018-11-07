11463b3580db6166bc60a2a91787f01fc8924963
Kyra Edwards <44348375+kyraedwards@users.noreply.github.com>
2018-11-06 19:20:51 -0800

    Update main.cpp
    Set size on window

--- main.cpp
+++ main.cpp
@@ -13,14 +13,17 @@ int main(int argc, char *argv[])
     FitFriend w;
     QWidget *wid = new QWidget;
     w.show();
+    w.setBaseSize(100, 80);
 
 
+//sets up main layout
     QVBoxLayout* overall_layout= new QVBoxLayout;
     QLabel* Intro = new QLabel("Hi, I'm your Fit Friend");
     QFont intro_font("Helvetica", QFont::Bold,  18);
        Intro->setFont(intro_font);
     overall_layout->addWidget(Intro);
     overall_layout->setAlignment(Intro, Qt::AlignCenter);
+
 
 
     //will hold the options on the left side of the main menu
@@ -35,17 +38,23 @@ int main(int argc, char *argv[])
 
 
 
+    //adds all of the buttons to the main menu
     for(size_t i=0; i < main_menu.length(); ++i){
         options->addWidget(main_menu[i]);
     }
 
 
     //input menu
-    QObject::connect(main_menu[0], SIGNAL(clicked()), &w, SLOT(FitFriend::input_data()));
+    QObject::connect(main_menu[0], SIGNAL(clicked()), &w, SLOT(input_data()));
 
-               QPixmap runners(":/../../../../../Downloads/download.jpeg");
-               QLabel* picture= new QLabel;
-              picture->setPixmap(runners);
+
+       //right hand side picture
+      QPixmap runners(":/success");
+      QLabel* picture= new QLabel;
+      picture->setPixmap(runners);
+      picture->setScaledContents(true);
+    picture->setFixedSize(400, 200);
+
 
     //will layout the buttons on the left side of the main menu and the photo on the right side of the main menu
     QHBoxLayout* options_and_picture = new QHBoxLayout;
@@ -56,18 +65,18 @@ int main(int argc, char *argv[])
 
 
 
+    //adds quit button to the bottom of the front page.
     QPushButton* quit = new QPushButton("QUIT");
     overall_layout->addWidget(quit);
     overall_layout->setAlignment(quit, Qt::AlignRight);
     QObject::connect(quit, SIGNAL(clicked()), &a, SLOT(quit()) );
-
-
 
     wid->setLayout(overall_layout);
     w.setCentralWidget(wid);
 
 
 
+
+
     return a.exec();
 }
-
