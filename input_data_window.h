9279586ff4475e2e786779275819b0a4b5ec765d
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-08 15:18:12 -0800

    Create input_data_window.h

---
+++ input_data_window.h
@@ -0,0 +1,48 @@
+#ifndef INPUT_DATA_WINDOW_H
+#define INPUT_DATA_WINDOW_H
+
+
+#include <QMainWindow>
+#include <fitfriend.h>
+
+namespace Ui {
+class input_data_window;
+}
+
+class input_data_window : public QMainWindow
+{
+    Q_OBJECT
+
+
+public slots:
+
+    //opens window that gives user the options of different types of fitness data to input
+ //   void input_data();
+
+    //opens window to allow user to input workout
+    void add_workout();
+
+    //opens window to allow user to input meal
+    void add_meal();
+
+    //opens window to allow user to input run
+    void add_run();
+
+    //opens window to allow user to input weight
+    void add_weight();
+
+
+public:
+    explicit input_data_window(QWidget *parent = nullptr);
+    void input_data();
+    ~input_data_window();
+
+
+
+
+private:
+    Ui::FitFriend *ui;
+};
+
+
+#endif // INPUT_DATA_WINDOW_H
