47c6e4b0f7d1bdc6235a2261e24a334e2ba291c8
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-08 15:17:37 -0800

    Update fitfriend.h

--- fitfriend.h
+++ fitfriend.h
@@ -13,9 +13,21 @@ class FitFriend : public QMainWindow
 
 
 public slots:
-    void input_data();
-    void add_workout();
 
+      //opens window that gives user the options of different types of fitness data to input
+      void input_data();
+
+//    //opens window to allow user to input workout
+ //  void add_workout();
+
+//    //opens window to allow user to input meal
+      //void add_meal();
+
+//    //opens window to allow user to input run
+//    void add_run();
+
+//    //opens window to allow user to input weight
+//    void add_weight();
 
 
 public:
