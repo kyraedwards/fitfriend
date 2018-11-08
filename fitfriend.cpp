3a241622ee1d79b5ddaa284a06bc194b37c9eceb
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-08 15:17:14 -0800

    Update fitfriend.cpp

--- fitfriend.cpp
+++ fitfriend.cpp
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QPushButton>
+#include "input_data_window.h"
 
 FitFriend::FitFriend(QWidget *parent) :
     QMainWindow(parent),
@@ -29,42 +30,70 @@ FitFriend::~FitFriend()
 }
 
 
-void FitFriend::input_data(){
+void FitFriend::input_data() {
 
-    FitFriend w;
-    w.show();
+    //window to host data input options
+    input_data_window* data_input_window = new input_data_window;
+
+    data_input_window->show();
+    data_input_window->setWindowTitle("Input data here!");
 
 
-    QLabel* welcome = new QLabel("Hello! What do you want to input?");
-    QPushButton* button = new QPushButton("Push me");
+    //introductory label
+    QLabel* intro_label = new QLabel("Select what you wish to input.");
+
+
+    //buttons to add different types of fitness data
+    QPushButton* add_meal_button = new QPushButton("Meal");
+    QPushButton* add_workout_button = new QPushButton("Workout");
+    QPushButton* add_weight_button = new QPushButton("Weight");
+    QPushButton* add_run_button = new QPushButton("Run");
+
+
+    //connect 4 add buttons to their respective SLOTS
+    QObject::connect(add_meal_button, SIGNAL(pressed()), data_input_window, SLOT(add_meal()));
+    QObject::connect(add_workout_button, SIGNAL(pressed()), data_input_window, SLOT(add_workout()));
+    QObject::connect(add_weight_button, SIGNAL(pressed()), data_input_window, SLOT(add_weight()));
+    QObject::connect(add_run_button, SIGNAL(pressed()), data_input_window, SLOT(add_run()));
+
+
+    //layout to structure the intro label and buttons to add fitness data
+    QVBoxLayout* label_and_buttons = new QVBoxLayout;
+    label_and_buttons->addWidget(intro_label);
+    label_and_buttons->addWidget(add_meal_button);
+    label_and_buttons->addWidget(add_workout_button);
+    label_and_buttons->addWidget(add_weight_button);
+    label_and_buttons->addWidget(add_run_button);
+
+
+    //hosts entire data input window
     QWidget* wid = new QWidget;
-    QVBoxLayout* layout= new QVBoxLayout;
-    //welcome->show();
-
-    wid->setLayout(layout);
-    layout->addWidget(welcome);
-    layout->addWidget(button);
-
-    wid->show();
-
-    QObject::connect(button, SIGNAL(clicked()), &w, SLOT(Add_Workout()) );
-
-
-
-
-    //why dont these lines of code work?
-//    w.setCentralWidget(wid);
-
-   // w.setLayout(layout);
-
-   //  w.show();
-
+    wid->setLayout(label_and_buttons);
+    data_input_window->setCentralWidget(wid);
 
 
 }
 
-void FitFriend::add_workout(){
-    QLabel* workout= new QLabel("Add yout workout");
-    workout->show();
 
-};
+
+//void FitFriend::add_workout(){
+
+//    QLabel* workout= new QLabel("Add yout workout");
+//    workout->show();
+
+//};
+
+//void FitFriend::add_meal(){
+//    QLabel* meal= new QLabel("Add yout meal");
+//    meal->show();
+//}
+
+//void FitFriend::add_run(){
+//    QLabel* run= new QLabel("Add yout run");
+//    run->show();
+//}
+
+//void FitFriend::add_weight(){
+//    QLabel* weight= new QLabel("Add yout weight");
+//    weight->show();
+//}
