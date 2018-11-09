8ecf57f3a178d7b9560123b70f95e33bdc864b06
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-08 21:10:11 -0800

    Update fitfriend.h

--- fitfriend.h
+++ fitfriend.h
@@ -29,10 +29,23 @@ public slots:
       //opens window that gives user the options of different types of fitness data to input
       void open_input_data();
 
-      //open the "advice from kyra" window
+      //open the "advice from kyra" window from main menu
       void open_advice_from_kyra();
 
+      //open the "my progress" window from main menu
+      void open_my_progress();
+
+      //open the "previous workouts" window from main menu
+      void open_previous_workouts();
+
+      //open "plan my route" window from main menu
+      void open_plan_my_route();
+
+      //open "set my goals" window from main menu
+      void open_set_my_goals();
+
 public:
+
     explicit FitFriend(QWidget *parent = nullptr);
     ~FitFriend();
 
