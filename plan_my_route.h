1d31101c41b730d8d242098f9004d9fbe1568619
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:01:24 -0800

    Update plan_my_route.h

--- plan_my_route.h
+++ plan_my_route.h
@@ -5,6 +5,8 @@
 #include <QObject>
 #include <QWidget>
 #include "fitfriend.h"
+#include <QVBoxLayout>
+#include <QPushButton>
 
 namespace Ui {
 class plan_my_route ;
@@ -16,17 +18,21 @@ class plan_my_route : public QMainWindow
 
 public:
 
-    explicit plan_my_route(QWidget *parent = nullptr);
+    explicit plan_my_route(QWidget *parent = nullptr, FitFriend* i_parent_window = nullptr);
     ~plan_my_route();
 
 signals:
 
+
 public slots:
+
+    //exits window and returns to main menu of app
+    void return_to_main();
 
 private:
     Ui::FitFriend *ui;
+    FitFriend* parent_window;
 
 };
 
 #endif // PLAN_MY_ROUTE_H
-
