be19464a0793c867ffe7ba4ee2457d2c17f5493c
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-15 17:30:05 -0800

    Update input_data_window.cpp

--- input_data_window.cpp
+++ input_data_window.cpp
@@ -55,17 +55,57 @@ input_data_window::~input_data_window()
 
 
 
-
 void input_data_window::add_workout(){
 
-    QLabel* workout= new QLabel("Add yout workout");
-    workout->show();
+
+    QLabel* workout= new QLabel("Congrats on finishing your workout! Add the workout info below");
+    QLabel* time_and_date = new QLabel("What were the time and date of this workout");
+    QLabel* time_of_workout = new QLabel("How long was your workout");
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(workout);
+    layout->addWidget(time_and_date);
+    layout->addWidget(time_of_workout);
+
+    QWidget* wid = new QWidget;
+    wid->setLayout(layout);
+    this->setCentralWidget(wid);
 
 };
 
 void input_data_window::add_meal(){
-   QLabel* meal= new QLabel("Add yout meal");
-   meal->show();
+   //this layout will hold the question labels and the answer input boxes
+   QVBoxLayout* layout = new QVBoxLayout;
+
+
+   QLabel* meal= new QLabel("Add yout meal below");
+   //need corresponding input text boxes and such
+   QLabel* time_and_date = new QLabel("What were the time and date of this meal");
+   QLabel* name_of_food = new QLabel("What food did you eat");
+   QLabel* calories_of_food = new QLabel("How many calories were in the food");
+   QLabel* fat_of_food = new QLabel("How many grams of fat were in the food");
+   QLabel* protein_of_food = new QLabel("How many grams of protein were in the food");
+
+   //need corresponding input text boxes
+   QLabel* name_of_drink = new QLabel("What was your drink");
+   QLabel* calories_of_drink = new QLabel("How many calories were in the drink");
+   QLabel* fat_of_drink = new QLabel("How many grams of fat were in the drink");
+   QLabel* protein_of_drink = new QLabel("How many grams of protein were in the drink");
+
+   layout->addWidget(meal);
+   layout->addWidget(time_and_date);
+   layout->addWidget(name_of_food);
+   layout->addWidget(calories_of_food);
+   layout->addWidget(fat_of_food);
+   layout->addWidget(protein_of_food);
+   layout->addWidget(name_of_drink);
+   layout->addWidget(calories_of_drink);
+   layout->addWidget(fat_of_drink);
+   layout->addWidget(protein_of_drink);
+
+   QWidget* wid = new QWidget;
+   wid->setLayout(layout);
+   this->setCentralWidget(wid);
 }
 
 void input_data_window::add_run(){
