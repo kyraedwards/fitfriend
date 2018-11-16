a92ba14075454fbcb5b46eb9da3b453227059091
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:05:21 -0800

    Update my_progress.cpp

--- my_progress.cpp
+++ my_progress.cpp
@@ -1,12 +1,13 @@
 #include "my_progress.h"
 #include "fitfriend.h"
 #include "ui_fitfriend.h"
+#include <QPushButton>
 #include <QLabel>
 #include <QVBoxLayout>
 
 
-my_progress::my_progress(QWidget *parent) :
-    QMainWindow(parent),
+my_progress::my_progress(QWidget *parent, FitFriend* i_parent_window) :
+    QMainWindow(parent), parent_window(i_parent_window),
     ui(new Ui::FitFriend)
 {
     ui->setupUi(this);
@@ -14,9 +15,14 @@ my_progress::my_progress(QWidget *parent) :
 
     QLabel* graph_label = new QLabel("Graph of progress made coming soon");
     QLabel* intro_label = new QLabel("Here is all the progress you've made!");
+    QPushButton* exit = new QPushButton("Exit");
 
     layout->addWidget(intro_label);
     layout->addWidget(graph_label);
+    layout->addWidget(exit);
+
+    QObject::connect(exit, SIGNAL(clicked()), this, SLOT(return_to_main()));
+
 
     QWidget* wid = new QWidget;
     wid->setLayout(layout);
@@ -28,4 +34,9 @@ my_progress::my_progress(QWidget *parent) :
 my_progress::~my_progress()
 {
     delete ui;
+}
+
+void my_progress::return_to_main(){
+    this->close();
+    parent_window->show();
 }
