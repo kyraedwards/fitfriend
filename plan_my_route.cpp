42a6840b9eb7657d758e98afb46c46cf7e468893
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:05:39 -0800

    Update plan_my_route.cpp

--- plan_my_route.cpp
+++ plan_my_route.cpp
@@ -3,13 +3,30 @@
 #include "ui_fitfriend.h"
 #include <QLabel>
 
-plan_my_route::plan_my_route(QWidget *parent) :
-    QMainWindow(parent),
+plan_my_route::plan_my_route(QWidget *parent, FitFriend* i_parent_window) :
+    QMainWindow(parent), parent_window(i_parent_window),
     ui(new Ui::FitFriend)
 {
     ui->setupUi(this);
     QLabel* intro_label = new QLabel("Coming soon");
-    intro_label->show();
+    QPushButton* return_to_main_button = new QPushButton("Return to main");
+    QObject::connect(return_to_main_button, SIGNAL(clicked()), this, SLOT(return_to_main()));
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(intro_label);
+    layout->addWidget(return_to_main_button);
+
+    QWidget* wid = new QWidget;
+    wid->setLayout(layout);
+    setCentralWidget(wid);
+    this->show();
+
+}
+
+
+void plan_my_route::return_to_main(){
+    this->close();
+    parent_window->show();
 }
 
 
@@ -17,5 +34,3 @@ plan_my_route::~plan_my_route()
 {
     delete ui;
 }
-
-
