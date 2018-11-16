aa11b3fe21d130f59a53b2f5b1d6cfb8a5d87ae9
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:02:07 -0800

    Update set_my_goals.h

--- set_my_goals.h
+++ set_my_goals.h
@@ -5,9 +5,11 @@
 #include <QObject>
 #include <QWidget>
 #include "fitfriend.h"
+#include <QVBoxLayout>
+#include <QPushButton>
 
 namespace Ui {
-class previous_workouts ;
+class set_my_goals ;
 }
 
 class set_my_goals : public QMainWindow
@@ -15,15 +17,21 @@ class set_my_goals : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit set_my_goals(QWidget *parent = nullptr);
+    explicit set_my_goals(QWidget *parent = nullptr, FitFriend* i_parent_window = nullptr);
     ~set_my_goals();
 
 signals:
 
 public slots:
 
+    //exits set my goals window and returns to main menu of app
+    void return_to_main();
+
+
+
 private:
     Ui::FitFriend *ui;
+    FitFriend* parent_window;
 };
 
 #endif // SET_MY_GOALS_H
