dd3d628416ea007136aa187107ab1da930b1078f
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-15 17:31:11 -0800

    Update my_progress.cpp

--- my_progress.cpp
+++ my_progress.cpp
@@ -2,17 +2,26 @@
 #include "fitfriend.h"
 #include "ui_fitfriend.h"
 #include <QLabel>
+#include <QVBoxLayout>
 
 
 my_progress::my_progress(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::FitFriend)
 {
-
     ui->setupUi(this);
+    QVBoxLayout* layout = new QVBoxLayout;
 
-    QLabel* intro_label = new QLabel("Coming soon");
-    intro_label->show();
+    QLabel* graph_label = new QLabel("Graph of progress made coming soon");
+    QLabel* intro_label = new QLabel("Here is all the progress you've made!");
+
+    layout->addWidget(intro_label);
+    layout->addWidget(graph_label);
+
+    QWidget* wid = new QWidget;
+    wid->setLayout(layout);
+    setCentralWidget(wid);
+    this->show();
 }
 
 
@@ -20,5 +29,3 @@ my_progress::~my_progress()
 {
     delete ui;
 }
-
-
