878162564b8200de3370e5d64fed2e4634f49df9
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-15 17:32:46 -0800

    Update input_data_types.h

--- input_data_types.h
+++ input_data_types.h
@@ -8,6 +8,10 @@ public:
 //default
 Time();
 
+//copy ctor
+//member-wise copy
+Time(const Time& i_time);
+
 //3 parameters
 Time(int i_hours, int i_minutes, double i_seconds);
 
@@ -35,14 +39,16 @@ double m_seconds;
 
 };
 
-
-
 class Date{
 
 public:
 
     //default ctor
     Date();
+
+    //copy ctor
+    //member-wise copy
+    Date(const Date& i_date);
 
     //ctor with 3 parameters
     Date(int i_day, int i_month, int i_year);
@@ -55,161 +61,27 @@ private:
 
 };
 
-
 class input_data
 {
 public:
+
+    //default ctor
     input_data();
-};
 
-
-
-class Profile{
-public:
-
-    Profile();
-    Profile(double );
-    double to_imperial();
-    double to_metric();
-    double calc_cals(Time length);
-
-    //updates daily cals burned, used in update weight.
-    void update_daily_cals(double kgs);
-
-    //used in input weight to change profile.
-
-    void update_weight(double kgs){
-        weight= kgs;
-
-       //weight change will change daily cals and calc cals.
-        update_daily_cals(kgs);
-    };
-
-
-
-
-private:
-    friend class weight;
-    friend Workout;
-    friend Run;
-    double weight;
-    double height;
-    char sex;
-    char imperial;
-    int age;
-    double daily_cals;
-
-
-
-};
-
-
-
-class Run: public input_data{
-public:
-
-    //default, sets all vals =0
-    Run();
-
-    //constructs base class object and user input.
-    Run(const Time &start_time, const Date &start_date,  double d, const Time &len);
-
-
-
-
-private:
-    double distance, cals_burned;
-    Time length;
-
-
-};
-
-
-Run::Run():
-
-Run::Run(const Time &start_time, const Date & start_date, double d, double cals, const Time &len)
-:input_data(start_time,start_date),distance(d), length(len){
-    cals_burned= Profile::cals_burned(len);
-
-};
-
-
-
-//Meal
-
-
-class Meal: public input_data{
-public:
-
-    //construct, base classs objecy and member fields initialized.
-    Meal(const Time &start_time, const Date& start_date, std::string i_food, std::string i_bev, double i_food_cals=0,
-         double i_bev_cals=0, double i_food_fat=0,double i_bev_fat=0, double i_food_prot=0, double i_bev_prot=0);
-
-
- private:
-        std::string food, bev;
-        double food_cals, bev_cals, food_fat, bev_fat, food_prot, bev_prot;
-
-};
-
-
-Meal::Meal( const Time &start_time, const Date& start_date, std::string i_food, std::string i_bev, double i_food_cals=0,
-     double i_bev_cals=0, double i_food_fat=0,double i_bev_fat=0, double i_food_prot=0, double i_bev_prot=0 ):
-    input_data(start_time, start_date):food(i_food), bev(i_bev), food_cals(i_food_cals), bev_cals(i_bev_cals),
-    food_fat(i_food_fat), bev_fat(i_bev_fat), food_prot(i_food_prot), bev_prot(i_bev_prot){};
-
-
-class Weight: public input_data{
-public:
-    //defualt
-    Weight();
-    //gives daily weight to profile
-    void update_profile() ;
-
-    //constructer with base class
-
-   Weight(const Time &start_time, const Date & start_date, double w);
-
-
+    //ctor with 2 parameters
+    //set parameters to respective member variables
+    input_data(const Time& i_start_time, const Date& i_date);
 
 private:
 
-double kgs;
-};
-
-
-Weight::Weight(const Time &start_time, const Date & start_date, double w):input_data(start_time, start_date)kgs(w) {};
-Weight::update_profile(){
-
-
-}
-
-
-class Workout: public input_data{
-public:
-//default
-    Workout();
-//constructor w/ params
-    Workout(const Time &start_time, const Date& start_date, const Time &len);
-
-private:
-    //note, this member field is not initialized inconstructor, it is done through profile class fn, calc_cal.
-    //which can be called inside the constructor.
-    double cals_burned;
-    //user entered
-    Time length;
+    Time m_start_time;
+    Date m_date;
 
 
 };
 
-Workout(const Time &start_time, const Date& start_date, const Time &len){
 
-    cals_burned = Profile::calc_cals(len);
 
-};
+
 
 #endif // INPUT_DATA_TYPES_H
-
-
-
-
