ab48c7f4581a66f419d3cadd6fcfaac8f49d35e5
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:05:55 -0800

    Update previous_workouts.cpp

--- previous_workouts.cpp
+++ previous_workouts.cpp
@@ -3,18 +3,31 @@
 #include "ui_fitfriend.h"
 #include <QLabel>
 
-previous_workouts::previous_workouts(QWidget *parent) :
-    QMainWindow(parent),
+previous_workouts::previous_workouts(QWidget *parent, FitFriend* i_parent_window) :
+    QMainWindow(parent), parent_window(i_parent_window),
     ui(new Ui::FitFriend)
 {
 
     QLabel* intro_label = new QLabel("Coming soon");
-    intro_label->show();
+    QPushButton* return_to_main = new QPushButton("Return to main");
+    QObject::connect(return_to_main, SIGNAL(clicked()), this, SLOT(return_to_main()));
 
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(intro_label);
+    layout->addWidget(return_to_main);
+
+    QWidget* wid = new QWidget;
+    wid->setLayout(layout);
+    setCentralWidget(wid);
+    this->show();
 }
+
+void previous_workouts::return_to_main(){
+    this->close();
+    parent_window->show();
+}
+
 
 previous_workouts::~previous_workouts(){
     delete ui;
 }
-
-
