c7d7d78d41e24911303fafb08b6fc1a51e264b1d
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:01:46 -0800

    Update previous_workouts.h

--- previous_workouts.h
+++ previous_workouts.h
@@ -5,6 +5,8 @@
 #include <QObject>
 #include <QWidget>
 #include "fitfriend.h"
+#include <QPushButton>
+#include <QVBoxLayout>
 
 namespace Ui {
 class previous_workouts ;
@@ -15,15 +17,19 @@ class previous_workouts : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit previous_workouts(QWidget *parent = nullptr);
+    explicit previous_workouts(QWidget *parent = nullptr, FitFriend* i_parent_window = nullptr);
     ~previous_workouts();
 
 signals:
 
 public slots:
 
+    //exits window and returns to main menu of app
+    void return_to_main();
+
 private:
     Ui::FitFriend *ui;
+    FitFriend* parent_window;
 
 };
 
