c5f9b2d5430dc69a33693dd43e16da6395559770
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2018-11-16 14:01:02 -0800

    Update my_progress.h

--- my_progress.h
+++ my_progress.h
@@ -6,7 +6,6 @@
 #include <QWidget>
 #include "fitfriend.h"
 
-
 namespace Ui {
 class my_progress ;
 }
@@ -17,18 +16,20 @@ class my_progress : public QMainWindow
     Q_OBJECT
 public:
 
-    explicit my_progress(QWidget *parent = nullptr);
+    explicit my_progress(QWidget *parent = nullptr, FitFriend* i_parent_window = nullptr);
     ~my_progress();
 
 signals:
 
 public slots:
 
+    //exits my_progress and returns to the main menu
+    void return_to_main();
+
 private:
 
     Ui::FitFriend *ui;
+    FitFriend* parent_window;
 };
 
 #endif // MY_PROGRESS_H
-
-
